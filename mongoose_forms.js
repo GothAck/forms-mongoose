5f719c3e4f47427a9511c4917635e46a3a926915
Greg Miell <greg@greg-net.co.uk>
2012-02-26 22:40:56 +0000

    Reworked to allow access to both forms.create and our create

--- mongoose_forms.js
+++ mongoose_forms.js
@@ -2,32 +2,48 @@ var forms = require('forms')
   , fields = forms.fields
   , validators = forms.validators;
 
-module.exports = function (model) {
+module.exports.create = function (model) {
   var schema = model.schema
-    , tree = schema.tree
+    , paths = schema.paths
+    , virtuals = schema.virtuals
     , params = {};
-
-  for (var name in tree) {
-    var field = tree[name];
-    if (!field.forms_type || field.forms_ignore) continue;
-    params[name] = fields[field.forms_type]({
-      required: field.required || false
-    });
-    console.log(params);
+  for (var pathName in paths) {
+    var path = paths[pathName];
+    if (path.options && path.options.forms_type) {
+      params[pathName] = fields[path.options.forms_type]({
+        required: path.options.required || path.options.unique
+      });
+    }
+  }
+  for (var virtName in virtuals) {
+    var virt = virtuals[virtName];
+    if (virt.options) {
+      if (virt.options.forms_type) {
+        params[virtName] = fields[virt.options.forms_type]({
+          required: virt.options.forms_required
+        });
+      }
+      if (virt.options.forms_confirm) {
+        params[virtName+'.confirm'] = fields[virt.options.forms_type]({
+          required: virt.options.forms_required,
+          validators: [validators.matchField(virtName)]
+        });
+      }
+    }
   }
   return forms.create(params);
 }
 
 module.exports.fields = fields;
 module.exports.validators = validators;
-module.exports.create = forms.create;
+module.exports.createForm = forms.create;
 
 module.exports.bs_iterator = function (name, field) {
   var label = this.label || name[0].toUpperCase() + name.substr(1).replace('_', ' ');
   var html = '<div class="control-group'
     + (field.error ? ' error' : '')
     + '">'
-    + '<label class="control-label" for="'+field.id+'">'+label+'</label>'
+    + '<label class="control-label" for="'+(field.id || 'id_'+name)+'">'+label+'</label>'
     + '<div class="controls">'
     + field.widget.toHTML(name, field)
     + (field.error ? '<p class="help-block">' + field.error + '</p>' : '')
